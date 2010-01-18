746afc65e02e7e7ec69cb622c55cacf7e12e42a2
Joni Freeman <joni.freeman@reaktor.fi>
2010-01-18 18:54:41 +0200

    Add thread safety test.

--- lift-base/lift-json/src/test/scala/net/liftweb/json/ParserTest.scala
+++ lift-base/lift-json/src/test/scala/net/liftweb/json/ParserTest.scala
@@ -23,6 +23,16 @@ object ParserSpec extends Specification with JValueGen with ScalaCheck {
     forAll(genObject, bufSize, bufSize)(parsing) must pass
   }
 
+  "Parsing is thread safe" in {
+    import java.util.concurrent._
+
+    val json = Examples.person
+    val executor = Executors.newFixedThreadPool(100)
+    val results = (0 to 100).map(_ => 
+      executor.submit(new Callable[JValue] { def call = parse(json) })).toList.map(_.get)
+    results.zip(results.tail).forall(pair => pair._1 == pair._2) mustEqual true
+  }
+
   "All valid string escape characters can be parsed" in {
     parse("[\"abc\\\"\\\\\\/\\b\\f\\n\\r\\t\\u00a0\"]") must_== JArray(JString("abc\"\\/\b\f\n\r\t\u00a0")::Nil)
   }
