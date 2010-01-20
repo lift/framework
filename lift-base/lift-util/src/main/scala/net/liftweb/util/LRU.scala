28c65e089bf82dd23e2baffa0c17203da38d855a
David Pollak <dpp@sparrow.(none)>
2010-01-20 14:42:23 -0800

    Created a new LRU implementation and moved it to common

--- lift-base/lift-util/src/main/scala/net/liftweb/util/LRU.scala
+++ lift-base/lift-util/src/main/scala/net/liftweb/util/LRU.scala
@@ -13,9 +13,9 @@ package net.liftweb.util
  * limitations under the License.
  */
 
-import _root_.org.apache.commons.collections.map.{LRUMap, AbstractLinkedMap}
-import _root_.org.apache.commons.collections.map.AbstractLinkedMap.LinkEntry
-import _root_.scala.collection.jcl.Conversions._
+//import _root_.org.apache.commons.collections.map.{LRUMap, AbstractLinkedMap}
+//import _root_.org.apache.commons.collections.map.AbstractLinkedMap.LinkEntry
+//import _root_.scala.collection.jcl.Conversions._
 import common._
 
 /**
@@ -24,9 +24,11 @@ import common._
  * @param size the maximum number of Elements allowed in the LRU map
  * @param loadFactor the Load Factor to construct our LRU with.
  */
-class LRU[KeyType, ValueType](size: Int, loadFactor: Box[Float]) {
+class LRU[KeyType, ValueType](size: Int, loadFactor: Box[Float]) extends net.liftweb.common.LRU(size, loadFactor) {
   // Alternate constructor that gives you no load factor.
   def this(size: Int) = this(size, Empty)
+
+  /*
 
   private val map = loadFactor match {
     case Full(lf) => new LRUMap(size, lf)
@@ -46,4 +48,5 @@ class LRU[KeyType, ValueType](size: Int, loadFactor: Box[Float]) {
   def apply(k: KeyType): ValueType = map.get(k).asInstanceOf[ValueType]
   def contains(k: KeyType): Boolean = map.containsKey(k)
   def keys: List[KeyType] = map.keySet().toList.map(_.asInstanceOf[KeyType])
+  */
 }
