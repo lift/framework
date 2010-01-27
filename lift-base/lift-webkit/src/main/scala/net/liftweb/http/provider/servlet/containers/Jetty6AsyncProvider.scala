c6217436457a0ff5a9d00f5bd2e1e22ec4cd3cdc
David Pollak <feeder.of.the.bears@gmail.com>
2010-01-27 10:20:23 -0800

    Added copyright notice and fixed package stuff for the async providers

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/provider/servlet/containers/Jetty6AsyncProvider.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/provider/servlet/containers/Jetty6AsyncProvider.scala
@@ -1,4 +1,24 @@
-package net.liftweb.http.provider.servlet.containers
+/*
+ * Copyright 2010 WorldWide Conferencing, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package net.liftweb {
+package http {
+package provider {
+package servlet {
+package containers {
 
 import _root_.javax.servlet.http.HttpServletRequest
 
@@ -76,4 +96,9 @@ class Jetty6AsyncProvider(req: HTTPRequest) extends ServletAsyncProvider {
   }
 
 
+}
+}
+}
+}
+}
 }
