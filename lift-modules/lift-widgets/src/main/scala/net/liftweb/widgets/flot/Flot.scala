1d61d6c851c0bfc4f9ba95c96dd3bae8eb70bef5
Peter Robinett <peter@bubblefoundry.com>
2010-02-04 15:19:57 -0800

    Have lift-flot use the unpacked version of excanvas.js provided by Flot 0.6

--- lift-modules/lift-widgets/src/main/scala/net/liftweb/widgets/flot/Flot.scala
+++ lift-modules/lift-widgets/src/main/scala/net/liftweb/widgets/flot/Flot.scala
@@ -50,7 +50,7 @@ object Flot
     ResourceServer.allow({
         case "flot" :: "jquery.flot.css" :: Nil => true
         case "flot" :: "jquery.flot.js" :: Nil => true
-        case "flot" :: "excanvas.pack.js" :: Nil => true
+        case "flot" :: "excanvas.js" :: Nil => true
       })
   }
 
@@ -72,7 +72,7 @@ object Flot
   {
     val ieExcanvasPackJs = Unparsed("<!--[if IE]><script language=\"javascript\" type=\"text/javascript\" src=\"" +
                                     net.liftweb.http.S.contextPath + "/" +
-                                    LiftRules.resourceServerPath + "/flot/excanvas.pack.js\"></script><![endif]-->")
+                                    LiftRules.resourceServerPath + "/flot/excanvas.js\"></script><![endif]-->")
 
     // 27/06/2009 add style tag )See http://groups.google.com/group/liftweb/browse_thread/thread/5e0335583e2a248b?hl=en&pli=1)
 
@@ -83,7 +83,7 @@ object Flot
     def renderHead(): NodeSeq = {
     val ieExcanvasPackJs = Unparsed("<!--[if IE]><script language=\"javascript\" type=\"text/javascript\" src=\"" +
                                     net.liftweb.http.S.contextPath + "/" +
-                                    LiftRules.resourceServerPath + "/flot/excanvas.pack.js\"></script><![endif]-->")
+                                    LiftRules.resourceServerPath + "/flot/excanvas.js\"></script><![endif]-->")
 
     // 27/06/2009 add style tag )See http://groups.google.com/group/liftweb/browse_thread/thread/5e0335583e2a248b?hl=en&pli=1)
 
