23dea2d68d3e7d2b75ed3bf35a83f9ca39b815a7
David Pollak <feeder.of.the.bears@gmail.com>
2010-02-22 14:23:55 -0800

    Closes #357.  Deal with changes in how Nodes are compared in 2.8

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/js/Jx.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/js/Jx.scala
@@ -36,7 +36,7 @@ trait JxBase {
 
   def appendToParent(parentName: String): JsCmd
 
-  def label = throw new UnsupportedOperationException("Xml2Js does not have a label")
+  def label = "jx" // throw new UnsupportedOperationException("Xml2Js does not have a label")
 
   def addAttrs(varName: String, attrs: List[MetaData]): JsCmd = attrs.map {
     m =>
