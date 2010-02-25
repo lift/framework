3a072b2969d092efef83f3fc74d1133e9c69494c
David Pollak <feeder.of.the.bears@gmail.com>
2010-02-25 11:27:05 -0800

    Closes #319.  Added tests to make sure that PCData stuff is not molested by the enhanced additions to the search for control characters that might make it into XML

--- lift-base/lift-util/src/main/scala/net/liftweb/util/PCDataMarkupParser.scala
+++ lift-base/lift-util/src/main/scala/net/liftweb/util/PCDataMarkupParser.scala
@@ -249,6 +249,15 @@ object AltXML {
   x match {
     case Text(str) => escape(str, sb)
 
+    case c: PCData => c.toString(sb)
+
+    case c: scala.xml.PCData => c.toString(sb)
+
+    case up: Unparsed => up.toString(sb)
+
+    case a: Atom[_] if a.getClass eq classOf[Atom[_]] =>
+      escape(a.data.toString, sb)
+
     case c: Comment if !stripComment =>
       c.toString(sb)
 
@@ -319,6 +328,15 @@ object AltXML {
             ieMode: Boolean): Unit =
   x match {
     case Text(str) => escape(str, sb)
+
+    case c: PCData => c.toString(sb)
+
+    case c: scala.xml.PCData => c.toString(sb)
+
+    case up: Unparsed => up.toString(sb)
+
+    case a: Atom[_] if a.getClass eq classOf[Atom[_]] =>
+      escape(a.data.toString, sb)
       
     case c: Comment if !stripComment =>
       c.toString(sb)
