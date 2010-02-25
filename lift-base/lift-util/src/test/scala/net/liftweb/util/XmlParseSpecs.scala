d9fa433853e5582c824c04fc4e878dec7a2dc62f
David Pollak <feeder.of.the.bears@gmail.com>
2010-02-25 11:31:06 -0800

    Revert "C #319.  Added tests to make sure that PCData stuff is not molested by the enhanced additions to the search for control characters that might make it into XML"
    Whoops... forgot to create a branch first.
    
    This reverts commit 565ae6731775a671806cc44e7dfd933e3822e8bf.

--- lift-base/lift-util/src/test/scala/net/liftweb/util/XmlParseSpecs.scala
+++ lift-base/lift-util/src/test/scala/net/liftweb/util/XmlParseSpecs.scala
@@ -20,7 +20,6 @@ package util {
 import _root_.org.specs._
 import _root_.org.specs.runner._
 import common._
-import scala.xml._
 
 class ToXmlParseTest extends JUnit4(XmlParseSpecs)
 object XmlParseSpecs extends Specification {
@@ -42,55 +41,6 @@ object XmlParseSpecs extends Specification {
       parsed must ==/(expected)
     }
 
-  }
-
-  "XML can contain PCData" in {
-    val data = <foo>{
-        PCData("Hello Yak")
-      }</foo>
-
-    val str = AltXML.toXML(data, false, true)
-
-    str.indexOf("<![CDATA[") must be > -1
-  }
-
-  "XML can contain Unparsed" in {
-    val data = <foo>{
-        Unparsed("Hello & goodbye > <yak Yak")
-      }</foo>
-
-    val str = AltXML.toXML(data, false, true)
-
-    str.indexOf("Hello & goodbye > <yak Yak") must be > -1
-  }
-
-  "XML cannot contain Control characters" in {
-     val data = 
-     <foo>
-      {
-        '\u0085'
-      }{
-        Text("hello \u0000 \u0085 \u0080")
-      }{
-        "hello \u0000 \u0003 \u0085 \u0080"
-      }{
-        '\u0003'
-      }
-    </foo>
-
-    val str = AltXML.toXML(data, false, true)
-
-    def cntIllegal(in: Char): Int = in match {
-      case '\u0085' => 1
-      case c if (c >= '\u007f' && c <= '\u0095') => 1
-      case '\n' => 0
-      case '\r' => 0
-      case '\t' => 0
-      case c if c < ' ' => 1
-      case _ => 0
-    }
-
-    str.toList.foldLeft(0)((a, b) => a + cntIllegal(b)) must_== 0
   }
 
 }
