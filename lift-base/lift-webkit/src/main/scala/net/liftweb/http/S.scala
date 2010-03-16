8e74e127114eb785c02bb0e1c3f6eaf08079b010
David Pollak <feeder.of.the.bears@gmail.com>
2010-03-16 12:31:51 -0700

    Use reflection on a method that's not available in JDK 1.5

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/S.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/S.scala
@@ -598,7 +598,16 @@ object S extends HasParams {
     _resBundle.box match {
       case Full(Nil) => {
         _resBundle.set(LiftRules.resourceNames.flatMap(name => tryo{
-              if (Props.devMode) {ResourceBundle.clearCache}
+              if (Props.devMode) {
+                tryo{
+                  val clz = this.getClass.getClassLoader.loadClass("java.util.ResourceBundle")
+                  val meth = clz.getDeclaredMethods.
+                  filter{m => m.getName == "clearCache" && m.getParameterTypes.length == 0}.
+                  toList.head
+                
+                  meth.invoke(null)
+                }
+              }
           List(ResourceBundle.getBundle(name, locale))
         }.openOr(
           NamedPF.applyBox((name, locale), LiftRules.resourceBundleFactories.toList).map(List(_)) openOr Nil
