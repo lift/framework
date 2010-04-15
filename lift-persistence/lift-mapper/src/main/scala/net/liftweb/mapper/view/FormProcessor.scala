c032d3ef1f7acc4c57ed3504d13ae18fab0c203c
Indrajit Raychaudhuri <irc@indrajit.com>
2010-04-15 16:51:25 +0530

    Scala 2.8: MapLike#keys returns Iterable[B] instead of Iterator[B]

--- lift-persistence/lift-mapper/src/main/scala/net/liftweb/mapper/view/FormProcessor.scala
+++ lift-persistence/lift-mapper/src/main/scala/net/liftweb/mapper/view/FormProcessor.scala
@@ -32,38 +32,38 @@ import net.liftweb.common.{Full, Empty}
  * @author nafg
  */
 abstract class FormProcessor(prefix: String) {
-  
+
   def text(attrs: (String, String)*)(init: String, action: String=>Unit) = SHtml.text(init, action, attrs:_*)
   def text: (String,String=>Unit)=>NodeSeq  = text()_
   def checkbox(attrs: (String, String)*)(init: Boolean, action: Boolean=>Unit) = SHtml.checkbox(init, action, attrs:_*)
   def checkbox: (Boolean, Boolean=>Unit)=>NodeSeq = checkbox()_
-  
+
   val stringValues: mMap[String, String] = mMap.empty[String, String]
-  val strings = mMap.empty[String, (String,String=>Unit)=>NodeSeq] 
+  val strings = mMap.empty[String, (String,String=>Unit)=>NodeSeq]
 
   val booleanValues = mMap.empty[String, Boolean]
   val booleans = mMap.empty[String, (Boolean,Boolean=>Unit)=>NodeSeq]
-  
 
-  
-  
+
+
+
   def bind(xhtml: NodeSeq) = {
     def transform(node: NodeSeq): NodeSeq = {
       put
       node match {
-      	case Elem(`prefix`, label, _, _, _*) if strings.keys contains label =>
-      	  strings(label)(stringValues(label), stringValues(label) = _)
-      	case Elem(`prefix`, label, _, _, _*) if booleans.keys contains label =>
+        case Elem(`prefix`, label, _, _, _*) if strings.keys.toIterator contains label =>
+          strings(label)(stringValues(label), stringValues(label) = _)
+        case Elem(`prefix`, label, _, _, _*) if booleans.keys.toIterator contains label =>
           booleans(label)(booleanValues(label), booleanValues(label) = _)
-       case other => other
+        case other => other
       }
     }
     Helpers.bind(prefix, Full(transform _), Empty, xhtml) ++
       Seq(SHtml.hidden(()=>get))
-    
+
   }
 
-  
+
   def put: Unit
   def get: Unit
 }
