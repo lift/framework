bb8dde035a214414f74f0f6ccab11f4fcc9a285b
Indrajit Raychaudhuri <irc@indrajit.com>
2010-04-15 15:45:58 +0530

    Scala 2.8: MapLike#values returns Iterable[B] instead of Iterator[B]

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/LiftMerge.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/LiftMerge.scala
@@ -39,7 +39,7 @@ private[http] trait LiftMerge {
     def waitUntilSnippetsDone() {
       val myMillis = millis
       snippetHashs.synchronized {
-        if (myMillis >= waitUntil || snippetHashs.isEmpty || !snippetHashs.values.contains(Empty)) ()
+        if (myMillis >= waitUntil || snippetHashs.isEmpty || !snippetHashs.values.toIterator.contains(Empty)) ()
         else {
           snippetHashs.wait(waitUntil - myMillis)
           waitUntilSnippetsDone()
