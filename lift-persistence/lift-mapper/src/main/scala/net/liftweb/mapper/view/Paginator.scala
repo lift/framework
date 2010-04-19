0196268656e92c390e79f5ef17b9bc8f18ce68e9
Timothy Perrett <hello@timperrett.com>
2010-04-20 00:39:39 +0100

    Updated scala docs on new paginator

--- lift-persistence/lift-mapper/src/main/scala/net/liftweb/mapper/view/Paginator.scala
+++ lift-persistence/lift-mapper/src/main/scala/net/liftweb/mapper/view/Paginator.scala
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.liftweb {
 package mapper {
 package view {
@@ -26,23 +25,71 @@ package view {
   import net.liftweb.mapper.{Mapper, MetaMapper, MappedField,
            QueryParam, OrderBy, StartAt, MaxRows, Ascending, Descending}
   import net.liftweb.mapper.view.ModelSnippet
-
+  
+  /**
+   * Snippet helper for people implementing 
+   * pagination - this ensures they implement the paginator
+   * 
+   * @author Timothy Perrett and nafg
+   */ 
   trait PaginatedSnippet extends DispatchSnippet {
+    override def dispatch = super.dispatch orElse Map("paginate" -> paginator.paginate _ )
     val paginator: Paginator
   }
-
+  
+  /**
+   * Base trait for anything that should exhibit paginating abilities.
+   * The key fields here are:
+   * - "num" that holds the number of items per page
+   * - "count" holds the overall item count
+   * 
+   * Then we have a bunch of methods you can override to change things:
+   * - firstXml is the Elem for the first page
+   * - lastXml is the Elem for the last page
+   * - prevXml is the Elem for the previous page
+   * - nextXml is the Elem for the next page
+   * - recordsXml is the Elem a handy message notifiying the users which records are showing
+   * - calcActiveLink is the Elem for active links
+   * - calcPassiveLink is the Elem for unclickable (current) links
+   * 
+   * View XHTML is as follows.
+   * sort prefix
+   *  - (a header name passed to the constructor) - a link that sorts by the field specified in the constructor
+   * nav prefix
+   *  - first - a link to the first page
+   *  - prev - a link to the previous page
+   *  - allpages - links to individual pages. The contents of this node are used to separate page links.
+   *  - next - a link to the next page
+   *  - last - a link to the last page
+   *  - records - a description of which records are currently being displayed
+   *
+   * @author Timothy Perrett and nafg
+   */ 
   trait Paginator extends Loggable {
-
+    
+    /**
+     * The query offset of the current page
+     */
     var _first: Long = 0L
     def first(): Long = _first
     def first(x: Long): Long = {  
       _first = x 
       first() 
     }
-
+    
+    /**
+     * The number of items on a page
+     */
     def num = 20
+    
+    /**
+     * Returns the total number of items to list
+     */
     protected def count: Long
-
+    
+    /**
+     * Configuration points for the XML components
+     */ 
     def firstXml = Text(?("<<"))
     def lastXml = Text(?(">>"))
     def prevXml = Text(?("<"))
@@ -71,7 +118,11 @@ package view {
         // first(start)
         calcActiveLink(start,ns)
       }
-
+    
+    
+    /**
+     * Link calculation methods - override this in a specilization
+     */
     def calcActiveLink(start: Long, xml: NodeSeq): NodeSeq 
     def calcPassiveLink(start: Long, xml: NodeSeq): NodeSeq
 
@@ -81,7 +132,22 @@ package view {
     def prevOffset = first()-num max 0
     def lastOffset = num*(numPages-1)
     def firstOffset = 0
-
+    
+    
+    /**
+     * This is the paginate snippet. It provides page
+     * navigation and column sorting links.
+     * View XHTML is as follows.
+     * sort prefix
+     *  - (a header name passed to the constructor) - a link that sorts by the field specified in the constructor
+     * nav prefix
+     *  - first - a link to the first page
+     *  - prev - a link to the previous page
+     *  - allpages - links to individual pages. The contents of this node are used to separate page links.
+     *  - next - a link to the next page
+     *  - last - a link to the last page
+     *  - records - a description of which records are currently being displayed
+     */
     def paginate(xhtml: NodeSeq) = 
       bind("nav", xhtml,
         "first" -> linkIfOther(firstOffset, firstXml),
@@ -101,25 +167,50 @@ package view {
       )
 
   }
-
+  
+  /**
+   * Specilized paginator that deals with Mapper instances 
+   * and knows how to automaticlly find the count from a MetaMapper
+   *
+   * @param meta The singleton to query for items
+   * @param initialSort Selection of MappedField's that will be use to sort the data set
+   * @param headers Pairs (Tuple2) of column header names as used in the view and the fields that they sort by
+   * @author Timothy Perrett and nafg
+   */
   class MapperPaginator[T <: Mapper[T]](val meta: MetaMapper[T],
                            initialSort: MappedField[_, T],
                            val headers: (String, MappedField[_, T])*) extends Paginator {
 
     var sort: OrderBy[T, _] = OrderBy(initialSort, Ascending)
-    var constantParams: Seq[QueryParam[T]] = Nil
+    
+    /**
+     * Override this to specify unchanging QueryParams to query the listing
+     */
+    def constantParams: Seq[QueryParam[T]] = Nil
 
     override def first(): Long = S.param("offset").map(_.toLong) openOr 0L
 
-    override def calcActiveLink(start: Long, xml: NodeSeq): NodeSeq = {
+    override def calcActiveLink(start: Long, xml: NodeSeq): NodeSeq = 
       <a href={"?offset=" + start}>{xml}</a>
-    }
-
     override def calcPassiveLink(start: Long, xml: NodeSeq): NodeSeq = 
       <span>{xml}</span>
-
+    
+    /**
+     * Returns the total number of items to list 
+     * by calling the MetaMapper
+     */
     def count = meta.count(constantParams: _*)
+    
+    /**
+     * The current data set for this page
+     */
     def page: List[T] = meta.findAll(constantParams ++ Seq(sort, MaxRows(num), StartAt(first())): _*)
+    
+    /**
+     * Sets the sort-by field. If the specified field is already
+     * the sort field, it is sorted descending; otherwise ascending.
+     * Called when a column header is clicked.
+     */
     def sortBy(field: MappedField[_, T]) = sort = sort match {
       case OrderBy(f, Ascending) if f eq field =>
         OrderBy(field, Descending)
@@ -127,7 +218,12 @@ package view {
         OrderBy(field, Ascending)
     }
   }
-
+  
+  /**
+   * Further specilization for Mapper uses where if you 
+   * are using ModelView stuff submitted by nafg then you can 
+   * auto-paginate that calling into the StatefulSnippet 
+   */
   class MapperViewPaginator[T <: Mapper[T]](meta: MetaMapper[T], 
           snippet: ModelSnippet[T], initialSort: MappedField[_, T], headers: (String, MappedField[_, T])*) 
         extends MapperPaginator[T](meta,initialSort,headers: _*){
