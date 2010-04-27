2f114de518f10a3e9125a1b22bfbf5adb4220566
David Pollak <feeder.of.the.bears@gmail.com>
2010-04-27 13:41:54 -0700

    Closes #467.  Exceptions while processing views are surfaced like Snippet exceptions

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/LiftRules.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/LiftRules.scala
@@ -996,11 +996,11 @@ object LiftRules extends Factory with FormVendor with LazyLoggable {
   @volatile var exceptionHandler = RulesSeq[ExceptionHandlerPF].append {
     case (Props.RunModes.Development, r, e) =>
       logger.error("Exception being returned to browser when processing " + r.uri.toString + ": " + showException(e))
-      XhtmlResponse((<html> <body>Exception occured while processing{r.uri}<pre>{showException(e)}</pre> </body> </html>), ResponseInfo.docType(r), List("Content-Type" -> "text/html; charset=utf-8"), Nil, 500, S.ieMode)
+      XhtmlResponse((<html> <body>Exception occured while processing {r.uri}<pre>{showException(e)}</pre> </body> </html>), ResponseInfo.docType(r), List("Content-Type" -> "text/html; charset=utf-8"), Nil, 500, S.ieMode)
 
     case (_, r, e) =>
       logger.error("Exception being returned to browser when processing " + r, e)
-      XhtmlResponse((<html> <body>Something unexpected happened while serving the page at{r.uri}</body> </html>), ResponseInfo.docType(r), List("Content-Type" -> "text/html; charset=utf-8"), Nil, 500, S.ieMode)
+      XhtmlResponse((<html> <body>Something unexpected happened while serving the page at {r.uri}</body> </html>), ResponseInfo.docType(r), List("Content-Type" -> "text/html; charset=utf-8"), Nil, 500, S.ieMode)
   }
 
   /**
