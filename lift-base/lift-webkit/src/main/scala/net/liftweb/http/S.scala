4ee3070439893b600a7511d94fe6b259590f418c
David Pollak <feeder.of.the.bears@gmail.com>
2010-04-29 14:02:39 -0700

    Closes #499.  Creates a mechanism for purging those pesky search engine created sessions

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/S.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/S.scala
@@ -2145,7 +2145,7 @@ for {
         case _ =>
           val ret = LiftSession(httpRequest.session, req.contextPath)
           ret.fixSessionTime()
-          SessionMaster.addSession(ret)
+          SessionMaster.addSession(ret, httpRequest.userAgent, Full(httpRequest.remoteAddress))
           ret
       }
 
