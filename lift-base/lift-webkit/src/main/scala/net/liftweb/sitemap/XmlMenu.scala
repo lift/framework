43fde60b64261dd81b92a58568cea760536711f3
Indrajit Raychaudhuri <irc@indrajit.com>
2010-05-21 21:30:14 +0530

    Scala 2.8.RC2: Iterator#collect does something different in 2.8, use toSeq instead

--- lift-base/lift-webkit/src/main/scala/net/liftweb/sitemap/XmlMenu.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/sitemap/XmlMenu.scala
@@ -35,7 +35,7 @@ object XmlMenu {
   def apply(xml: scala.xml.NodeSeq): Seq[Menu] = for(node<-xml) yield node match {
     case m @ <menu>{ children @ _* }</menu> =>
       val name = m \ "@name" text
-      val text = scala.xml.NodeSeq.fromSeq((m \ "text" elements) flatMap {_.child.elements} collect)
+      val text = scala.xml.NodeSeq.fromSeq((m \ "text" elements) flatMap {_.child.elements} toSeq)
       val link = net.liftweb.util.JSONParser.parse(m \ "link" text).get.asInstanceOf[List[Any]].map(_.asInstanceOf[String])
       Menu(Loc(name, link, text), apply(m \ "menu") : _*)
   }
