fe75a20f7514b992cba38bc1669c8d59742335dc
David Pollak <feeder.of.the.bears@gmail.com>
2010-06-17 10:01:59 -0700

    DB reset on each test in MappedLongForeingKey

--- lift-persistence/lift-mapper/src/test/scala/net/liftweb/mapper/MappedLongForeignKeySpec.scala
+++ lift-persistence/lift-mapper/src/test/scala/net/liftweb/mapper/MappedLongForeignKeySpec.scala
@@ -29,8 +29,24 @@ class MappedLongForeignKeySpecAsTest extends JUnit3(MappedLongForeignKeySpec)
 object MappedLongForeignKeySpecRunner extends ConsoleRunner(MappedLongForeignKeySpec)
 
 object MappedLongForeignKeySpec extends Specification {
+  def provider = DBProviders.H2MemoryProvider
+
+  def doLog = false
+
+  private def ignoreLogger(f: => AnyRef): Unit = ()
+
+  def cleanup() {
+    try { provider.setupDB } catch { case e if !provider.required_? => skip("Provider %s not available: %s".format(provider, e)) }
+    Schemifier.destroyTables_!!(DefaultConnectionIdentifier, if (doLog) Schemifier.infoF _ else ignoreLogger _,  SampleTag, SampleModel, Dog, Mixer, Dog2, User)
+    Schemifier.destroyTables_!!(DBProviders.SnakeConnectionIdentifier, if (doLog) Schemifier.infoF _ else ignoreLogger _, SampleTagSnake, SampleModelSnake)
+    Schemifier.schemify(true, if (doLog) Schemifier.infoF _ else ignoreLogger _, DefaultConnectionIdentifier, SampleModel, SampleTag, User, Dog, Mixer, Dog2)
+    Schemifier.schemify(true, if (doLog) Schemifier.infoF _ else ignoreLogger _, DBProviders.SnakeConnectionIdentifier, SampleModelSnake, SampleTagSnake)
+  }
+
+
   "MappedLongForeignKey" should {
     "Not allow comparison to another FK" in {
+      cleanup()
       val dog = Dog.create.name("Froo").saveMe
       val user = {
         def ret: User = {
@@ -49,6 +65,7 @@ object MappedLongForeignKeySpec extends Specification {
     }
 
     "be primed after setting a reference" in {
+      cleanup()
       val dog = Dog.create
       val user = User.create
       dog.owner(user)
@@ -56,6 +73,7 @@ object MappedLongForeignKeySpec extends Specification {
     }
     
     "be primed after setting a Boxed reference" in {
+      cleanup()
       val dog = Dog.create
       val user = User.create
       dog.owner(Full(user))
@@ -63,6 +81,7 @@ object MappedLongForeignKeySpec extends Specification {
     }
     
     "be empty after setting an Empty" in {
+      cleanup()
       
       val user = User.create
       val dog = Dog.create.owner(user)
