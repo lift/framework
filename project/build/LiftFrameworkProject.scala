a00c97eb07d79649eb6556c1bf907cda144d1d5b
Indrajit Raychaudhuri <irc@indrajit.com>
2010-09-02 16:46:18 +0530

    fix incorrect ordering of traits in the same dependency chain; closes #632 for real

--- project/build/LiftFrameworkProject.scala
+++ project/build/LiftFrameworkProject.scala
@@ -111,41 +111,29 @@ class LiftFrameworkProject(info: ProjectInfo) extends LiftParentProject(info) {
 
 
 /**
- * Basic trait common for all the Lift project definitions.
- * This trait would be applied to both LiftParentProject as well as LiftDefaultProject.
+ * ParentProject specialized for Lift.
  */
-trait BasicLiftProject extends BasicDependencyProject {
+abstract class LiftParentProject(info: ProjectInfo) extends ParentProject(info) {
 
   // Custom format for java.net.URL
   implicit lazy val urlFormat = new SimpleFormat[URL] { def fromString(s: String) = new URL(s) }
 
   // Additional user-defined properties that optionally can be defined for the project.
-  lazy val projectUrl               = propertyOptional[URL](new URL("http://www.liftweb.net"), true)
-  lazy val projectInceptionyear     = propertyOptional[Int](C.getInstance().get(C.YEAR), true)
-  lazy val projectOrganizationName  = propertyOptional[String](organization, true)
-  lazy val projectOrganizationUrl   = propertyOptional[URL](projectUrl.value, true)
+  lazy val projectUrl                 = propertyOptional[URL](new URL("http://www.liftweb.net"), true)
+  lazy val projectInceptionyear       = propertyOptional[Int](C.getInstance().get(C.YEAR), true)
+  lazy val projectOrganizationName    = propertyOptional[String](organization, true)
+  lazy val projectOrganizationUrl     = propertyOptional[URL](projectUrl.value, true)
 
-  lazy val projectLicenseName       = propertyOptional[String]("Apache License, Version 2.0", true)
-  lazy val projectLicenseUrl        = propertyOptional[URL](
-    if (isApacheLic) new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")
-    else projectUrl.value, true)
+  lazy val projectLicenseName         = propertyOptional[String]("Apache License, Version 2.0", true)
+  lazy val projectLicenseUrl          = propertyOptional[URL](new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"), true)
   lazy val projectLicenseDistribution = propertyOptional[String]("repo", true)
 
-  private def isEmptyLic = projectLicenseName.value.trim.isEmpty
-  private def isApacheLic = projectLicenseName.value.startsWith("Apache") // TODO: Enrich
-
   lazy val publishRemote = propertyOptional[Boolean](false, true)
-
-//  override def disableCrossPaths = true
 
   // Add ScalaToolsSnapshots if this project is on snapshot
   override def repositories =
     if (version.toString.endsWith("-SNAPSHOT")) super.repositories + ScalaToolsSnapshots
     else super.repositories
-
-  // Add Maven Local repository for SBT to search for
-  // This is useful for resolving parent POM only; SBT does the needful of looking into (maven-local) for dependencies.
-  //val mavenLocal = "Local Maven Repository" at "file://" + (Path.userHome / ".m2" / "repository").absolutePath
 
   // Set up publish repository (the tuple avoids SBT's ReflectiveRepositories detection)
   private lazy val snapshotPublishRepo = ("Distribution Repository for Snapshots" -> "http://nexus.scala-tools.org/content/repositories/snapshots/")
@@ -163,40 +151,6 @@ trait BasicLiftProject extends BasicDependencyProject {
     if (!publishRemote.value) Some(localDestRepo)
     else super.defaultPublishRepository
 
-  // Enrich the generated POM
-  // TODO: add <url/> conditionally via pomPostProcess
-  private lazy val pomBasic: NodeSeq =
-    (<url>{projectUrl.value}</url>
-    <inceptionYear>{projectInceptionyear.value}</inceptionYear>
-    <organization>
-      <name>{projectOrganizationName.value}</name>
-      <url>{projectOrganizationUrl.value}</url>
-    </organization>)
-
-  private lazy val pomLic: NodeSeq =
-    if (isEmptyLic) NodeSeq.Empty
-    else
-      (<licenses>
-         <license>
-           <name>{projectLicenseName.value}</name>
-           <url>{projectLicenseUrl.value}</url>
-           <distribution>{projectLicenseDistribution.value}</distribution>
-           <comments>{name} is licensed under {projectLicenseName.value}</comments>
-         </license>
-       </licenses>)
-
-  override def pomExtra = pomBasic ++ pomLic
-
-  // Enforce Maven managedStyle [FIXME: this is necessary to publish pom during 'sbt publish', but it breaks 'sbt update' when using dependencies from pom.xml]
-  // override def managedStyle = ManagedStyle.Maven
-
-}
-
-
-/**
- * ParentProject specialized for Lift.
- */
-abstract class LiftParentProject(info: ProjectInfo) extends ParentProject(info) with BasicLiftProject {
 
   // Disable dependencies on sub-projects
   override def deliverProjectDependencies = Nil
@@ -207,15 +161,48 @@ abstract class LiftParentProject(info: ProjectInfo) extends ParentProject(info) 
 /**
  * DefaultProject specialized for Lift.
  */
-abstract class LiftDefaultProject(info: ProjectInfo) extends DefaultProject(info) with BasicLiftProject {
+abstract class LiftDefaultProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  // Custom format for java.net.URL
+  implicit lazy val urlFormat = new SimpleFormat[URL] { def fromString(s: String) = new URL(s) }
+
+  // Additional user-defined properties that optionally can be defined for the project.
+  lazy val projectUrl                 = propertyOptional[URL](new URL("http://www.liftweb.net"), true)
+  lazy val projectInceptionyear       = propertyOptional[Int](C.getInstance().get(C.YEAR), true)
+  lazy val projectOrganizationName    = propertyOptional[String](organization, true)
+  lazy val projectOrganizationUrl     = propertyOptional[URL](projectUrl.value, true)
+
+  lazy val projectLicenseName         = propertyOptional[String]("Apache License, Version 2.0", true)
+  lazy val projectLicenseUrl          = propertyOptional[URL](new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"), true)
+  lazy val projectLicenseDistribution = propertyOptional[String]("repo", true)
+
+  lazy val publishRemote = propertyOptional[Boolean](false, true)
+
+  // Add ScalaToolsSnapshots if this project is on snapshot
+  override def repositories =
+    if (version.toString.endsWith("-SNAPSHOT")) super.repositories + ScalaToolsSnapshots
+    else super.repositories
+
+  // Set up publish repository (the tuple avoids SBT's ReflectiveRepositories detection)
+  private lazy val snapshotPublishRepo = ("Distribution Repository for Snapshots" -> "http://nexus.scala-tools.org/content/repositories/snapshots/")
+  private lazy val releasePublishRepo  = ("Distribution Repository for Releases"  -> "http://nexus.scala-tools.org/content/repositories/releases/")
+
+  val publishTo =
+    if (version.toString.endsWith("-SNAPSHOT")) snapshotPublishRepo._1 at snapshotPublishRepo._2
+    else releasePublishRepo._1 at releasePublishRepo._2
+
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+  // Tell SBT to publish to local Maven repository unless publish.remote=true
+  private lazy val localDestRepo = Resolver.file("maven-local", Path.userHome / ".m2" / "repository" asFile)
+  override def defaultPublishRepository =
+    if (!publishRemote.value) Some(localDestRepo)
+    else super.defaultPublishRepository
+
 
   // Compile options [TODO: add additional debug options conditionally]
   override def compileOptions =
-//    super.compileOptions ++ Seq(Unchecked) ++ Seq("-Xmigration", "-encoding", "utf8").map(x => CompileOption(x))
-    Seq(Unchecked) ++ Seq("-Xmigration", "-encoding", "utf8").map(x => CompileOption(x))
-
-  // Test options
-  // override def testOptions = super.testOptions ++ TODO
+    Seq(Unchecked) ++ Seq("-encoding", "utf8").map(x => CompileOption(x))
 
   // Package options
   lazy val extraSpecificationEntries = ManifestAttributes(
@@ -244,10 +231,5 @@ abstract class LiftDefaultProject(info: ProjectInfo) extends DefaultProject(info
 
   // Make `package` depend on `test`
   override def packageAction = super.packageAction dependsOn testAction
-
-  // Publish sources [FIXME: this breaks 'sbt update' when using dependencies from pom.xml]
-  // override def packageSrcJar = defaultJarPath("-sources.jar")
-  // lazy val sourceArtifact = Artifact.sources(artifactID)
-  // override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
 
 }
