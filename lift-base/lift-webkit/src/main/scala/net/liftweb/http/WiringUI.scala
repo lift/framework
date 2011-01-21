c2700a7d53a3ce76b45ccbff314f507115a05465
David Pollak <feeder.of.the.bears@gmail.com>
2011-01-20 22:44:48 -0800

    Added some helpers to wiring to support designer friendly stuff

--- lift-base/lift-webkit/src/main/scala/net/liftweb/http/WiringUI.scala
+++ lift-base/lift-webkit/src/main/scala/net/liftweb/http/WiringUI.scala
@@ -42,6 +42,21 @@ object WiringUI {
   def apply[T](in: NodeSeq, cell: Cell[T])(f: T => NodeSeq => NodeSeq): NodeSeq = toNode(in, cell)((t, ns) => f(t)(ns))
 
   /**
+   * Given a Cell and a function that can generate
+   * a NodeSeq => NodeSeq from the cell's value, return a function that
+   * takes a NodeSeq and registers the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param cell the cell to associate with
+   * @param f the function that performs the drawing
+   *
+   * @return a function that mutates NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def apply[T](cell: Cell[T])(f: T => NodeSeq => NodeSeq): NodeSeq => NodeSeq = (in: NodeSeq) => toNode(in, cell)((t, ns) => f(t)(ns))
+
+  /**
    * Given a NodeSeq, a Cell and a function that can generate
    * a NodeSeq => NodeSeq from the cell's value, register the
    * postPageJavaScript that will update the element with
@@ -65,6 +80,30 @@ object WiringUI {
   def apply[T](in: NodeSeq, cell: Cell[T], jsEffect: (String, Boolean, JsCmd) => JsCmd)(f: T => NodeSeq => NodeSeq): NodeSeq = toNode(in, cell, jsEffect)((t, ns) => f(t)(ns))
 
   /**
+   * Given a Cell and a function that can generate
+   * a NodeSeq => NodeSeq from the cell's value, return a function that with a NodeSeq
+   * will register the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param cell the cell to associate with
+   * 
+   * @param jsEffect a function that wraps the SetHtml JsCmd so
+   * you can, for example, fade out the old value, set the new value and
+   * fade it in.  The first parameter is the id of the element, the
+   * second is a flag that's true if this is the first time the element is
+   * being rendered (you might want to skip effects for the inital page load),
+   * and the third parameter is the SetHtml JavaScript code.
+   * 
+   * @param f the function that performs the drawing
+   *
+   * @return the mutated NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def apply[T](cell: Cell[T], jsEffect: (String, Boolean, JsCmd) => JsCmd)(f: T => NodeSeq => NodeSeq): NodeSeq => NodeSeq = 
+    in => toNode(in, cell, jsEffect)((t, ns) => f(t)(ns))
+
+  /**
    * Given a NodeSeq, a Cell and a function that can generate
    * a NodeSeq from the cell's value and the template value, register the
    * postPageJavaScript that will update the element with
@@ -80,6 +119,21 @@ object WiringUI {
   def toNode[T](in: NodeSeq, cell: Cell[T])(f: (T, NodeSeq) => NodeSeq): NodeSeq = toNode(in, cell, (id, first, js) => js)(f)
 
   /**
+   * Given a NodeSeq, a Cell and a function that can generate
+   * a NodeSeq from the cell's value and the template value, register the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param in the NodeSeq that contains the view markup
+   * @param cell the cell to associate with
+   * @param f the function that performs the drawing
+   * 
+   * @return the mutated NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def toNode[T](cell: Cell[T])(f: (T, NodeSeq) => NodeSeq): NodeSeq => NodeSeq = in => toNode(in, cell, (id, first, js) => js)(f)
+
+  /**
    * Given a NodeSeq, a Cell register the
    * postPageJavaScript that will update the element with
    * a new value.
@@ -92,6 +146,19 @@ object WiringUI {
    */
   def asText[T](in: NodeSeq, cell: Cell[T]): NodeSeq = 
     toNode(in, cell, (id, first, js) => js)((t, ns) => Text(t.toString))
+
+  /**
+   * Given a Cell register the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param cell the cell to associate with
+   * 
+   * @return the mutated NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def asText[T](cell: Cell[T]): NodeSeq => NodeSeq = 
+    in => toNode(in, cell, (id, first, js) => js)((t, ns) => Text(t.toString))
 
   /**
    * Given a NodeSeq, a Cell register the
@@ -113,6 +180,27 @@ object WiringUI {
    */
   def asText[T](in: NodeSeq, cell: Cell[T], jsEffect: (String, Boolean, JsCmd) => JsCmd): NodeSeq = 
     toNode(in, cell, jsEffect)((t, ns) => Text(t.toString))
+
+  /**
+   * Given a NodeSeq, a Cell register the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param in the NodeSeq that contains the view markup
+   * @param cell the cell to associate with
+   * 
+   * @param jsEffect a function that wraps the SetHtml JsCmd so
+   * you can, for example, fade out the old value, set the new value and
+   * fade it in.  The first parameter is the id of the element, the
+   * second is a flag that's true if this is the first time the element is
+   * being rendered (you might want to skip effects for the inital page load),
+   * and the third parameter is the SetHtml JavaScript code.
+   * 
+   * @return the mutated NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def asText[T](cell: Cell[T], jsEffect: (String, Boolean, JsCmd) => JsCmd): NodeSeq => NodeSeq = 
+    in => toNode(in, cell, jsEffect)((t, ns) => Text(t.toString))
 
   /**
    * Given a NodeSeq, a Cell and a function that can generate
@@ -147,6 +235,40 @@ object WiringUI {
     })
     elem
   }
+
+  /**
+   * Given a Cell and a function that can generate
+   * a NodeSeq from the cell's value and the template value, register the
+   * postPageJavaScript that will update the element with
+   * a new value.
+   *
+   * @param cell the cell to associate with
+   * 
+   * @param jsEffect a function that wraps the SetHtml JsCmd so
+   * you can, for example, fade out the old value, set the new value and
+   * fade it in.  The first parameter is the id of the element, the
+   * second is a flag that's true if this is the first time the element is
+   * being rendered (you might want to skip effects for the inital page load),
+   * and the third parameter is the SetHtml JavaScript code.
+   * 
+   * @param f the function that performs the drawing
+   * 
+   * @return the mutated NodeSeq (an id attribute may be added if
+   * there's none already defined)
+   */
+  def toNode[T](cell: Cell[T], jsEffect: (String, Boolean, JsCmd) => JsCmd)(f: (T, NodeSeq) => NodeSeq): NodeSeq => NodeSeq =
+    in => {
+      val myElem: Elem = in.find {
+        case e: Elem => true
+        case _ => false
+      }.map(_.asInstanceOf[Elem]).getOrElse(<span id={Helpers.nextFuncName}>{in}</span>)
+      
+      val (elem: Elem, id: String) = Helpers.findOrAddId(myElem)
+      addJsFunc(cell, (t: T, first: Boolean) => {
+        jsEffect(id, first, SetHtml(id, f(t, elem.child)))
+      })
+      elem
+    }
 
   /**
    * Associate a Cell and a function that converts from the
