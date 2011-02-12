f917c8e4b2b7d28f92c0071317133172a8bd5e30
Indrajit Raychaudhuri <irc@indrajit.com>
2011-02-12 22:37:18 +0530

    Split liftsh options, modifying LIFTSH_OPTS is safer now. You can now do: LIFTSH_OPTS="-Dpublish.remote=true -Xmx3g" ./liftsh clean-plugins reload update publish

--- liftsh.cmd
+++ liftsh.cmd
@@ -1,9 +1,14 @@
 @echo off
 
-set DEFAULT_OPTS="-Dfile.encoding=UTF-8 -Xss8M -Xmx1G -noverify -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -Dfile.encoding=utf8 -Dsbt.intransitive=true"
+@REM Internal options, always specified
+set INTERNAL_OPTS="-Dfile.encoding=UTF-8 -Xss8M -Xmx1G -noverify -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:MaxPermSize=512M"
+
+@REM Default options, if nothing is specified
+set DEFAULT_OPTS="-Dsbt.intransitive=true"
 
 if "%LIFTSH_OPTS%"=="" (
   set LIFTSH_OPTS=DEFAULT_OPTS
 )
 
-java "%LIFTSH_OPTS%" -jar "%~dp0\project\sbt-launch-0.7.5.RC0.jar" %*
+@REM Call with INTERNAL_OPTS followed by LIFTSH_OPTS (or DEFAULT_OPTS). java aways takes the last option when duplicate.
+java %INTERNAL_OPTS% %LIFTSH_OPTS% -jar "%~dp0\project\sbt-launch-0.7.5.RC0.jar" %*
