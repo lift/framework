3f45e047ceb604ba676bde1902cc81d24bd765e5
Sami Dalouche <sami.dalouche@gmail.com>
2011-04-27 00:00:37 -0400

    json-ext: (joda) DateTime and DateMidnight are now restored as UTC

--- core/json-ext/src/test/scala/net/liftweb/json/ext/JodaTimeSerializerSpec.scala
+++ core/json-ext/src/test/scala/net/liftweb/json/ext/JodaTimeSerializerSpec.scala
@@ -35,7 +35,7 @@ object JodaTimeSerializerSpec extends Specification("JodaTimeSerializer Specific
 
   "Serialize joda time types" in {
     val x = JodaTypes(new Duration(10*1000), new Instant(System.currentTimeMillis),
-                      new DateTime, new DateMidnight, new Interval(1000, 50000),
+                      new DateTime(DateTimeZone.UTC), new DateMidnight(DateTimeZone.UTC), new Interval(1000, 50000),
                       new LocalDate(2011, 1, 16), new LocalTime(16, 52, 10), Period.weeks(3))
     val ser = swrite(x)
     read[JodaTypes](ser) mustEqual x
