0414b820da801a3a7011419d87fe9f31db16de42
David Pollak <feeder.of.the.bears@gmail.com>
2011-05-14 12:39:44 +0200

    Closes #1003.  More protouser enhancements including the ability to have multiple instances in a single app

--- persistence/proto/src/main/scala/net/liftweb/proto/ProtoRules.scala
+++ persistence/proto/src/main/scala/net/liftweb/proto/ProtoRules.scala
@@ -32,7 +32,7 @@ object ProtoRules extends Factory with LazyLoggable {
    * The regular expression pattern for matching email addresses.  This
    * assumes that the email address has been converted to lower case.
    */
-  var emailRegexPattern = new FactoryMaker(Pattern.compile("^[a-z0-9._%\\-+]+@(?:[a-z0-9\\-]+\\.)+[a-z]{2,4}$")) {}
+  val emailRegexPattern = new FactoryMaker(Pattern.compile("^[a-z0-9._%\\-+]+@(?:[a-z0-9\\-]+\\.)+[a-z]{2,4}$")) {}
   
 }
 
