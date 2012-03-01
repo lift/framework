9e27e1c03864a5fb60cdf77ec37ee40389e97d51
Indrajit Raychaudhuri <irc@indrajit.com>
2012-03-01 18:26:09 +0530

    - Update to SBT 0.12.0-M1 - Push down system property java.util.logging.config.file to more localized scope - Tweak liftsh properties to be more reliable and have lower footprint

--- liftsh.cmd
+++ liftsh.cmd
@@ -1,14 +1,14 @@
 @echo off
 
 @REM Internal options, always specified
-set INTERNAL_OPTS=-Dfile.encoding=UTF-8 -Xss8M -Xmx1G -noverify -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:MaxPermSize=512M
+set INTERNAL_OPTS=-Dfile.encoding=UTF-8 -Xmx768m -noverify -XX:ReservedCodeCacheSize=96m -XX:+UseCompressedOops -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:MaxPermSize=512m
 
 @REM Default options, if nothing is specified
-set DEFAULT_OPTS=-Dsbt.intransitive=true
+set DEFAULT_OPTS=
 
 if "%LIFTSH_OPTS%"=="" (
   set LIFTSH_OPTS=%DEFAULT_OPTS%
 )
 
-@REM Call with INTERNAL_OPTS followed by LIFTSH_OPTS (or DEFAULT_OPTS). java aways takes the last option when duplicate.
-java %INTERNAL_OPTS% %LIFTSH_OPTS% -jar "%~dp0\project\sbt-launch-0.11.2.jar" %*
+@REM Call with INTERNAL_OPTS followed by LIFTSH_OPTS (or DEFAULT_OPTS). java always takes the last option when duplicate.
+java %INTERNAL_OPTS% %LIFTSH_OPTS% -jar "%~dp0\project\sbt-launch-0.12.0-M1.jar" %*
