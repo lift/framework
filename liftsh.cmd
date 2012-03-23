d07d091f3c8f5b5400c6641c1595f54c82b9b225
Indrajit Raychaudhuri <irc@indrajit.com>
2012-03-23 16:01:32 +0530

    bump up `ReservedCodeCacheSize` for 64bit jvm

--- liftsh.cmd
+++ liftsh.cmd
@@ -6,7 +6,7 @@ set INTERNAL_OPTS=-Dfile.encoding=UTF-8 -Xmx768m -noverify -XX:ReservedCodeCache
 @REM Add 64bit specific option
 java -version 2>&1 | find "64-Bit" >nul:
 if not errorlevel 1 (
-  set INTERNAL_OPTS=%INTERNAL_OPTS% -XX:+UseCompressedOops
+  set INTERNAL_OPTS=%INTERNAL_OPTS% -XX:+UseCompressedOops -XX:ReservedCodeCacheSize=128m
 )
 
 @REM Default options, if nothing is specified
