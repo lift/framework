fbc3efb39864dbc8a9adda70861f82c0678806ab
Jeppe Nejsum Madsen <jeppe@ingolfs.dk>
2012-06-13 15:32:30 +0200

    Add YUI processor plugin to minify js resources

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 object PluginDef extends Build {
-  lazy val root              = Project("plugins", file(".")) dependsOn(buildPlugin)
+  lazy val root              = Project("plugins", file(".")) dependsOn(buildPlugin, yuiPlugin)
+  lazy val yuiPlugin	     = uri("git://github.com/indrajitr/sbt-yui-compressor")
   lazy val buildPlugin       = uri("git://github.com/indrajitr/sbt-lift-build-plugin.git#c78f617f62")
 }
