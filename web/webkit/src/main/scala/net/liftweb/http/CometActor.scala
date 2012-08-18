4b61567f2ee9225e770e8e299273f75b73a22ec5
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2012-08-18 14:37:20 -0400

    Add !< to LiftCometActor.
    CometActor had the function by virtue of extending Lift's actor traits;
    however, the LiftCometActor trait that is often used to reference it
    didn't define that the method would be present. We add it there so it
    can be referred to anywhere that LiftCometActor is passed around instead
    of CometActor.
    
    !< returns an LAFuture for a message reply.

--- web/webkit/src/main/scala/net/liftweb/http/CometActor.scala
+++ web/webkit/src/main/scala/net/liftweb/http/CometActor.scala
@@ -400,6 +400,12 @@ trait LiftCometActor extends TypedActor[Any, Any] with ForwardableActor[Any, Any
   }
 
   /**
+   * Asynchronous message send. Send-and-receive eventually. Returns a Future for the reply message.
+   */
+  def !<(msg: Any): LAFuture[Any]
+
+
+  /**
    * Override in sub-class to customise timeout for the render()-method for the specific comet
    */
   def cometRenderTimeout = LiftRules.cometRenderTimeout
