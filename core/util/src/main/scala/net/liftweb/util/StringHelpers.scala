f2de993c8c568c994d4b2a04daf22b21f459b7bf
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2012-10-11 16:36:08 -0400

    Use ThreadLocalRandom in StringHelpers.randomString on Java 7.
    On Java <7, we use the default SecureRandom, which locks on access.
    ThreadLocalRandom will not lock as it is thread-local (!).

--- core/util/src/main/scala/net/liftweb/util/StringHelpers.scala
+++ core/util/src/main/scala/net/liftweb/util/StringHelpers.scala
@@ -19,6 +19,7 @@ package util
 
 import java.security.SecureRandom
 import java.util.regex._
+import java.util.Random
 import java.lang.Character._
 import java.lang.{StringBuilder => GoodSB}
 import scala.xml.NodeSeq
@@ -32,7 +33,23 @@ object StringHelpers extends StringHelpers
 trait StringHelpers {
 
   /** random numbers generator */
-  private val _random = new SecureRandom
+  private lazy val _slowRandom = new SecureRandom
+  private lazy val _currentTlrMethod = {
+    try {
+      val tlr = Class.forName("java.util.concurrent.ThreadLocalRandom")
+      Full(tlr.getMethod("current"))
+    } catch {
+      case e =>
+      Failure("ThreadLocalRandom is not available.", Full(e), Empty)
+    }
+  }
+  private def withRng[T](block: (Random)=>T) = {
+    _currentTlrMethod.map { meth =>
+      block(meth.invoke(null).asInstanceOf[Random])
+    } openOr {
+      _slowRandom.synchronized(block(_slowRandom))
+    }
+  }
 
   /**
    * If str is surrounded by quotes it return the content between the quotes
@@ -176,7 +193,7 @@ trait StringHelpers {
       if (pos >= size) sb
       else {
         val randNum = if ((pos % 6) == 0) {
-          _random.synchronized(_random.nextInt)
+          withRng(_.nextInt)
         } else {
           lastRand
         }
