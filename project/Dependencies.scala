f2f1e83a170f23a08dac36e1401ce277368a5352
Dave Whittaker <dave@iradix.com>
2013-01-04 14:05:34 -0500

    Updated squeryl dependency to 0.9.5-6.  Closes #1386.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -48,7 +48,7 @@ object Dependencies {
   lazy val scala_compiler: ModuleMap  = "org.scala-lang"                % "scala-compiler"     % _
   lazy val scalaz_core: ModuleMap = sv => scalazGroup(sv)        % "scalaz-core"        % scalazVersion(sv) cross CVMappingAll
   lazy val slf4j_api              = "org.slf4j"                  % "slf4j-api"          % slf4jVersion
-  lazy val squeryl                = "org.squeryl"                % "squeryl"            % "0.9.5-4" cross crossMapped("2.9.1-1" -> "2.9.1", "2.8.2" -> "2.8.1")
+  lazy val squeryl                = "org.squeryl"                % "squeryl"           % "0.9.5-6" cross CVMapping29
 
   // Aliases
   lazy val mongo_driver = mongo_java_driver
