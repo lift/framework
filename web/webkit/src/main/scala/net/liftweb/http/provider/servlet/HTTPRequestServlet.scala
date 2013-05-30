5169861ef66d116fbaba118ddfce0876176d73ad
David Pollak <feeder.of.the.bears@gmail.com>
2013-05-30 09:31:56 -0700

    Null testing for headers. Closes #1452

--- web/webkit/src/main/scala/net/liftweb/http/provider/servlet/HTTPRequestServlet.scala
+++ web/webkit/src/main/scala/net/liftweb/http/provider/servlet/HTTPRequestServlet.scala
@@ -46,10 +46,18 @@ class HTTPRequestServlet(val req: HttpServletRequest, val provider: HTTPProvider
 
   lazy val authType: Box[String] = Box !! req.getAuthType
 
-  def headers(name: String): List[String] = enumToList[String](req.getHeaders(name).asInstanceOf[java.util.Enumeration[String]])
+  def headers(name: String): List[String] =
+    for {
+      h <- (Box !! req.getHeaders(name)).asA[java.util.Enumeration[String]].toList
+      li <- enumToList[String](h) if null != li
+    } yield li
 
-  lazy val headers: List[HTTPParam] = enumToList[String](req.getHeaderNames.asInstanceOf[java.util.Enumeration[String]]).
-          map(n => HTTPParam(n, headers(n)))
+  lazy val headers: List[HTTPParam] =
+    for {
+      hne <- (Box !! req.getHeaderNames).asA[java.util.Enumeration[String]].toList
+      n <- enumToList[String](hne) if null != n
+      hl <- Full(headers(n)) if !hl.isEmpty
+    } yield HTTPParam(n, hl)
 
   def contextPath: String = req.getContextPath
 
