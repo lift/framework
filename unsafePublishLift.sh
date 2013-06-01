db8b7fb3dc53f31f89145620dd8b107a02f32bde
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2013-06-01 00:16:38 -0400

    updated publishing script to build from last RC

--- unsafePublishLift.sh
+++ unsafePublishLift.sh
@@ -120,8 +120,8 @@ for MODULE in framework ; do
     CURRENT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     debug "Current branch for $MODULE is $CURRENT_BRANCH"
 
-    if [ "${CURRENT_BRANCH}" != "master" ]; then
-        echo "Currently releases can only be built from master. $MODULE is on branch $CURRENT_BRANCH. Aborting build."
+    if [ "${CURRENT_BRANCH}" != "2.5-RC6" ]; then
+        echo "Currently releases can only be built from the last RC, 2.5-RC6. $MODULE is on branch $CURRENT_BRANCH. Aborting build."
         exit
     fi
 
