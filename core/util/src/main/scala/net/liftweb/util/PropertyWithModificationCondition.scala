fb725cab748d18fb5eab9fd7c6f6e05642b77c55
David Barri <japgolly@gmail.com>
2013-06-29 09:19:41 +1000

    Split PropertyWithModificationCondition.set() into multiple lines and added doco.

--- core/util/src/main/scala/net/liftweb/util/PropertyWithModificationCondition.scala
+++ core/util/src/main/scala/net/liftweb/util/PropertyWithModificationCondition.scala
@@ -5,8 +5,24 @@ package net.liftweb.util
  */
 abstract class PropertyWithModificationCondition[T](initialValue: T) {
   @volatile private var value = initialValue
+
   def get = value
-  def set(newValue: T): Boolean = if (allowModification) {value = newValue; true} else {onModificationProhibited; false}
+
+  /**
+   * Attemps to set the property to a new value.
+   *
+   * @return Whether the new property was installed. `false` means modification is no longer allowed.
+   */
+  def set(newValue: T): Boolean =
+    if (allowModification) {
+      value = newValue
+      true
+    } else {
+      onModificationProhibited()
+      false
+    }
+
   def allowModification: Boolean
+
   def onModificationProhibited() {}
 }
