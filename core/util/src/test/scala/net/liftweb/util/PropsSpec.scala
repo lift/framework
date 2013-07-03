e43e8bd8f8e260d9397f34276ccee05914bbef52
David Barri <japgolly@gmail.com>
2013-07-04 09:46:28 +1000

    Added tests for run-mode property modification.

--- core/util/src/test/scala/net/liftweb/util/PropsSpec.scala
+++ core/util/src/test/scala/net/liftweb/util/PropsSpec.scala
@@ -18,17 +18,38 @@ package net.liftweb
 package util
 
 import org.specs2.mutable.Specification
-
+import Props.RunModes._
 
 /**
  * Systems under specification for Lift Mailer.
  */
 object PropsSpec extends Specification {
   "Props Specification".title
+  sequential
 
   "Props" should {
     "Detect test mode correctly" in {
       Props.testMode must_== true
+    }
+
+    "Allow modification of run-mode properties before the run-mode is set" in {
+      val before = Props.autoDetectRunModeFn.get
+      try {
+        Props.runModeInitialised = false
+        Props.autoDetectRunModeFn.allowModification must_== true
+        Props.autoDetectRunModeFn.set(() => Test) must_== true
+        Props.autoDetectRunModeFn.get must_!= before
+      } finally {
+        Props.autoDetectRunModeFn.set(before)
+        Props.runModeInitialised = true
+      }
+    }
+
+    "Prohibit modification of run-mode properties when the run-mode is set" in {
+      val before = Props.autoDetectRunModeFn.get
+      Props.autoDetectRunModeFn.allowModification must_== false
+      Props.autoDetectRunModeFn.set(() => Test) must_== false
+      Props.autoDetectRunModeFn.get must_== before
     }
   }
 }
