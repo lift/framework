021c43db4c7585a54de59f280c3837c8b6538786
Diego Medina <diego@fmpwizard.com>
2013-11-06 23:56:48 -0500

    Revert "Deleted JX"
    This reverts commit 849a376b156eb878711bbd422e0619b8ef79c742.
    
    Conflicts:
    	web/webkit/src/main/scala/net/liftweb/http/js/Jx.scala

--- persistence/mapper/src/main/scala/net/liftweb/mapper/MappedInt.scala
+++ persistence/mapper/src/main/scala/net/liftweb/mapper/MappedInt.scala
@@ -26,6 +26,7 @@ import java.util.Date
 import net.liftweb.http._
 import reflect.runtime.universe._
 import net.liftweb.json._
+import net.liftweb.http.jquery.{JqSHtml}
 import xml.{Text, NodeSeq}
 import js._
 
