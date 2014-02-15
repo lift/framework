bf56a6516701d94a53cd26111e67f9508c0069a2
Matt Farmer <matt@frmr.me>
2014-02-15 09:47:09 -0500

    Clarify some of the Memo names used in Meta.Reflection.
    I noticed when we merged a PR that the names of the various Memos in
    Meta.Reflection were getting a bit confusing, so I've cleaned them up a
    bit to 1) reduce confusion between what are now called
    hasDeclaredFieldsMemo and declaredFieldsMemo and 2) to clarify what the
    primaryConstructorArgumentsMemo actually contained.

--- core/json/src/main/scala/net/liftweb/json/Meta.scala
+++ core/json/src/main/scala/net/liftweb/json/Meta.scala
@@ -220,9 +220,9 @@ private[json] object Meta {
       classOf[java.lang.Short], classOf[Date], classOf[Timestamp], classOf[Symbol], classOf[JValue],
       classOf[JObject], classOf[JArray]).map((_, ())))
 
-    private val primaryConstructors = new Memo[Class[_], List[(String, Type)]]
-    private val declaredFields = new Memo[(Class[_], String), Boolean]
-    private val declaredFieldsMap = new Memo[Class[_], Map[String,Field]]
+    private val primaryConstructorArgumentsMemo = new Memo[Class[_], List[(String, Type)]]
+    private val hasDeclaredFieldsMemo = new Memo[(Class[_], String), Boolean]
+    private val declaredFieldsMemo = new Memo[Class[_], Map[String,Field]]
 
     def constructors(t: Type, names: ParameterNameReader, context: Option[Context]): List[(JConstructor[_], List[(String, Type)])] =
       rawClassOf(t).getDeclaredConstructors.map(c => (c, constructorArgs(t, c, names, context))).toList
@@ -267,7 +267,7 @@ private[json] object Meta {
         constructorArgs(c, primary, formats.parameterNameReader, None)
       }
 
-      primaryConstructors.memoize(c, findMostComprehensive(_))
+      primaryConstructorArgumentsMemo.memoize(c, findMostComprehensive(_))
     }
 
     def typeParameters(t: Type, k: Kind, context: Context): List[Class[_]] = {
@@ -358,7 +358,7 @@ private[json] object Meta {
 
     def getDeclaredFields(clazz: Class[_]) : Map[String,Field] = {
       def extractDeclaredFields = clazz.getDeclaredFields.map(field => (field.getName, field)).toMap
-      declaredFieldsMap.memoize(clazz, _ => extractDeclaredFields)
+      declaredFieldsMemo.memoize(clazz, _ => extractDeclaredFields)
     }
     
     def hasDeclaredField(clazz: Class[_], name: String): Boolean = {
@@ -369,7 +369,7 @@ private[json] object Meta {
         case e: NoSuchFieldException => false
       }
 
-      declaredFields.memoize((clazz, name), _ => declaredField)
+      hasDeclaredFieldsMemo.memoize((clazz, name), _ => declaredField)
     }
 
     def mkJavaArray(x: Any, componentType: Class[_]) = {
