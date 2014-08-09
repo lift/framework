26631912c9d1fef938fd483e5571a49e29a258c8
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-08-08 23:56:50 -0400

    Rework LazyLoad comet.
    We move the AsyncRenderComet to its own file. We also
    move the actual render function management into the
    AsyncRenderComet companion, including handling deferred
    function building.
    
    LazyLoad slims down a little, dealing with invoking the
    async render setup in AsyncRenderComet and rendering
    the placeholder.
    
    We also only apply a div wrapper to the default loading
    template, letting a custom template stand on its own, though
    we apply an id to it for the purposes of replacing it with the
    rendered contents after they are sent down.

--- web/webkit/src/main/scala/net/liftweb/builtin/snippet/LazyLoad.scala
+++ web/webkit/src/main/scala/net/liftweb/builtin/snippet/LazyLoad.scala
@@ -29,15 +29,13 @@ import JE._
 import S._
 import Helpers._
 
+import comet.AsyncRenderComet
 
 /**
  * Enclose your snippet tags on your template with LazyLoad and the snippet will execute
  * on a different thread, which avoids blocking the page render.
  */
 object LazyLoad extends DispatchSnippet {
-  private object myFuncName extends TransientRequestVar(Helpers.nextFuncName)
-  private object myActor extends TransientRequestVar[Box[CometActor]](Empty)
-
   def dispatch: DispatchIt = {
     case _ => render _
   }
@@ -45,110 +43,44 @@ object LazyLoad extends DispatchSnippet {
   /**
    * Enclose your snippet like this:
    *
-   * <pre name="code" class="xml">
-   *   &lt;div class="lift:LazyLoad">
-   *     &lt;div class="lift:MyLongRunningSnippet">&lt;/div>
-   *   &lt;/div>
-   * </pre>
+   * {{{
+   *   <div data-lift="lazy-load">
+   *     <div data-lift="MyLongRunningSnippet"></div>
+   *   </div>
+   * }}}
    *
-   * You can add the template attribute to the LazyLoad tag and instead of
-   * showing the spinning circle, it will render your template.
+   * You can also add the `template` attribute to the `lazy-load` snippet to
+   * specify what to render as a placeholder:
    *
+   * {{{
+   *   <div data-lift="lazy-load?template=my-nice-wait-message-template">
+   *     <div data-lift="MyLongRunningSnippet"></div>
+   *   </div>
+   * }}}
    *
-   * <pre name="code" class="xml">
-   *   &lt;div class="lift:LazyLoad?template=&#39;my-nice-wait-message-template&#39;">
-   *     &lt;div class="lift:MyLongRunningSnippet">&lt;/div>
-   *   &lt;/div>
-   * </pre>
-   *
-   *
+   * Note that this must be a single element that will be replaced when the lazy
+   * content is loaded. By default, an AJAX spinner is displayed inside a `div`.
    */
   def render(xhtml: NodeSeq): NodeSeq = {
-    (for {
-      session <- S.session ?~ ("FIXME: Invalid session")
-    } yield {
-      
-      // if we haven't created the actor yet, register on this
-      // thread to create the AsyncRenderComet actor
-      if (myActor.isEmpty) {
-        LiftRules.cometCreationFactory.request.set(
-          (ccinfo: CometCreationInfo) =>
-            ccinfo match {
-              case CometCreationInfo(theType @ "AsyncRenderComet",
-                                     name,
-                                     defaultXml,
-                                     attributes,
-                                     session) => {
-                val ret = new AsyncRenderComet()
-                ret.initCometActor(ccinfo)
-                ret ! PerformSetupComet2(if (ret.sendInitialReq_?) 
-       S.request.map(_.snapshot) else Empty)
-                
-                // and save it in the request var
-                myActor.set(Full(ret))
-                
-                Full(ret)
-              }
-              
-              case _ => Empty
-            })
-      }
+    val placeholderId = Helpers.nextFuncName
 
-      val id = Helpers.nextFuncName
-
-      val func: () => JsCmd = 
-        session.buildDeferredFunction(() => Replace(id, xhtml))
-
-      <div id={id}>
-      {
-        S.attr("template") match {
-          case Full(template) => <lift:embed what={template}/>
-          case _ => <img src="/images/ajax-loader.gif" alt="Loading"/>
+    AsyncRenderComet.asyncRender(()=>Replace(placeholderId, xhtml)).map { _ =>
+      ("^ [id]" #> placeholderId).apply(
+        {
+          for {
+            templatePath <- S.attr("template")
+            renderedTemplate <- S.eval(<lift:embed what={templatePath} />)
+          } yield {
+            renderedTemplate
+          }
+        } openOr {
+          <div><img src="/images/ajax-loader.gif" alt="Loading"/></div>
         }
-      }
-      </div>++ (myActor.is match {
-        case Full(actor) => actor ! Ready(func); NodeSeq.Empty
-        case _ => session.setupComet("AsyncRenderComet", Full(myFuncName.is), Ready(func))
-        <tail><lift:comet type="AsyncRenderComet" name={myFuncName.is}/></tail>
-      })
-    }) match {
-      case Full(x) => x
-      case Empty => Comment("FIX"+ "ME: session or request are invalid")
+      )
+    } match {
+      case Full(placeholderHtml) => placeholderHtml
       case Failure(msg, _, _) => Comment(msg)
+      case Empty => Comment("FIX"+"ME: Asynchronous rendering failed for unknown reason.")
     }
-
-  }
-
-}
-
-
-private case class Ready(js: () => JsCmd)
-private case class Render(js: JsCmd)
-
-
-/**
- * The Comet Actor for sending down the computed page fragments
- *
- */
-class AsyncRenderComet extends CometActor {
-
-  override def lifespan: Box[TimeSpan] = Full(90.seconds)
-
-  def render = NodeSeq.Empty
-
-  // make this method visible so that we can initialize the actor
-  override def initCometActor(creationInfo: CometCreationInfo) {
-    super.initCometActor(creationInfo)
-  }
-
-
-  override def lowPriority : PartialFunction[Any, Unit] = {
-    // farm the request off to another thread
-    case Ready(js) => 
-      Schedule.schedule(() => this ! Render(js()), 0.seconds)
-
-    // render it
-    case Render(js) => 
-      partialUpdate(js)
   }
 }
