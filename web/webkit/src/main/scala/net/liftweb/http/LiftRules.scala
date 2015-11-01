1fa8d06eb7e7aa93f70f83c953c35b6d2c456118
Joe Barnes <barnesjd@gmail.com>
2015-11-01 16:37:39 -0600

    Now using our default js object for logError, fixing issue #1720 a bit more elegantly IMO

--- web/webkit/src/main/scala/net/liftweb/http/LiftRules.scala
+++ web/webkit/src/main/scala/net/liftweb/http/LiftRules.scala
@@ -898,7 +898,7 @@ class LiftRules() extends Factory with FormVendor with LazyLoggable {
    *   LiftRules.jsLogFunc = Full(v => JE.Call("alert",v).cmd)
    */
   @volatile var jsLogFunc: Box[JsVar => JsCmd] =
-    if (Props.devMode) Full(v => JE.Call("lift.logError", v))
+    if (Props.devMode) Full(v => JE.Call("default.logError", v))
     else Empty
 
   /**
