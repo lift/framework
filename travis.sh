b914b15daad6e41aeb9644d5830d54637ecf7ebf
Matt Farmer <matt@frmr.me>
2015-11-03 09:30:57 -0500

    Implement the travis.sh build script.
    This build script for travis will check whether or not we're currently
    in a Travis PR build. If not, it'll check the branch. If we're on master
    or lift_26, we'll trigger a SNAPSHOT image deploy.
    
    Of course, we don't yet have the credentials in travis yet so this won't
    _quite_ work how we want it for the moment. But give me more than one
    commit to get that sorted out!
    
    :construction_worker:

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ev
+
+./liftsh test
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    sbt publish
+  fi
+
+  if [ "${TRAVIS_BRANCH}" = "lift_26" ]; then
+    sbt ++2.10.4 "project lift-framework-pre-111" publish
+    sbt ++2.11.1 publish
+  fi
+fi
