2687fe69969f0f4823dd05594ceec8a63f85640f
Matt Farmer <matt@frmr.me>
2015-11-03 10:26:24 -0500

    Use liftsh to attempt publish.

--- travis.sh
+++ travis.sh
@@ -5,14 +5,12 @@ set -ev
 ./liftsh test
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "Attempting publish!"
-
   if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    sbt publish
+    ./liftsh publish
   fi
 
   if [ "${TRAVIS_BRANCH}" = "lift_26" ]; then
-    sbt ++2.10.4 "project lift-framework-pre-111" publish
-    sbt ++2.11.1 publish
+    ./liftsh ++2.10.4 "project lift-framework-pre-111" publish
+    ./liftsh ++2.11.1 publish
   fi
 fi
