31335b4119ca0e6c3385daae84587174c934c29f
Joe Barnes <barnesjd@gmail.com>
2015-11-09 21:15:18 -0600

    Finished up FutureWithSession for the scala.concurrent.Futures

--- web/webkit/src/main/scala/net/liftweb/http/FutureWithSession.scala
+++ web/webkit/src/main/scala/net/liftweb/http/FutureWithSession.scala
@@ -1,7 +1,6 @@
 package net.liftweb.http
 
-import net.liftweb.common._
-import scala.concurrent._
+import scala.concurrent.{ExecutionContext, CanAwait, Future} 
 import scala.concurrent.duration.Duration
 import scala.util.Try
 
@@ -16,22 +15,34 @@ object FutureWithSession {
 class FutureWithSession[+T](future:Future[T])(implicit session:LiftSession) extends Future[T] {
   import FutureWithSession._
 
-  override def map[S](f: (T) ⇒ S)(implicit executor: ExecutionContext): Future[S] =
-    future.map ( t => S.initIfUninitted(session) ( f(t) )).withImplicitSession
-  override def flatMap[S](f: (T) ⇒ Future[S])(implicit executor: ExecutionContext): Future[S] =
-    future.flatMap ( t => S.initIfUninitted(session) ( f(t) )).withImplicitSession
-  override def andThen[U](pf: PartialFunction[Try[T], U])(implicit executor: ExecutionContext): Future[T] =
-    future.andThen { case t => S.initIfUninitted(session) ( pf(t) ) }.withImplicitSession
-  override def failed: Future[Throwable] = future.failed.withImplicitSession
-//  override def fallbackTo[U >: T](that: Future[U]): Future[U] = future.fallbackTo(that)
-
   // Override all of the abstract Future[T] stuff to pass thru
-  override def isCompleted = future.isCompleted
-  override def value = future.value
-  override def onComplete[U](f: (Try[T]) => U)(implicit executor:ExecutionContext) = future.onComplete(f)
-  override def result(atMost: Duration)(implicit permit: CanAwait) = future.result(atMost)
+  override def isCompleted:Boolean = future.isCompleted
+  override def value:Option[Try[T]] = future.value
+  override def result(atMost: Duration)(implicit permit: CanAwait):T = future.result(atMost)
   override def ready(atMost: Duration)(implicit permit: CanAwait) = {
     future.ready(atMost)
     this
   }
+  override def onComplete[U](f: (Try[T]) => U)(implicit executor:ExecutionContext):Unit =
+    future.onComplete( t => S.initIfUninitted(session) ( f(t) ) )
+
+  // Override all methods with functions that need to run in session scope and/or return other futures
+  override def andThen[U](pf: PartialFunction[Try[T], U])(implicit executor: ExecutionContext): Future[T] =
+    future.andThen { case t => S.initIfUninitted(session) ( pf(t) ) }.withImplicitSession
+  override def failed: Future[Throwable] = future.failed.withImplicitSession
+  override def fallbackTo[U >: T](that: Future[U]): Future[U] = future.fallbackTo(that).withImplicitSession
+  override def flatMap[S](f: (T) ⇒ Future[S])(implicit executor: ExecutionContext): Future[S] =
+    future.flatMap ( t => S.initIfUninitted(session) ( f(t) )).withImplicitSession
+  override def map[S](f: (T) ⇒ S)(implicit executor: ExecutionContext): Future[S] =
+    future.map ( t => S.initIfUninitted(session) ( f(t) )).withImplicitSession
+  override def recover[U >: T](pf: PartialFunction[Throwable, U])(implicit executor: ExecutionContext): Future[U] =
+    future.recover { case t => S.initIfUninitted(session) ( pf(t) ) }.withImplicitSession
+  override def recoverWith[U >: T](pf: PartialFunction[Throwable, Future[U]])(implicit executor: ExecutionContext): Future[U] =
+    future.recoverWith { case t => S.initIfUninitted(session) ( pf(t) ) }.withImplicitSession
+  override def transform[S](s: (T) ⇒ S, f: (Throwable) ⇒ Throwable)(implicit executor: ExecutionContext): Future[S] =
+    future.transform( t => S.initIfUninitted(session) ( s(t) ), t => S.initIfUninitted(session) ( f(t) ) ).withImplicitSession
+  override def zip[U](that: Future[U]): Future[(T, U)] = future.zip(that).withImplicitSession
+
+  // Note that the following methods are implemented reusing existing methods: filter, foreach, mapTo, onFailure, onSuccess
+  // We still have them covered by tests in case future changes to the Scala standard lib changes this.
 }
--- web/webkit/src/test/scala/net/liftweb/http/FutureWithSessionSpec.scala
+++ web/webkit/src/test/scala/net/liftweb/http/FutureWithSessionSpec.scala
@@ -9,8 +9,8 @@ import scala.util.{Try, Success}
 
 object FutureWithSessionSpec extends Specification {
 
-  object TestVar1 extends SessionVar[String]("test1")
-  object TestVar2 extends SessionVar[String]("test2")
+  object TestVar1 extends SessionVar[String]("Uninitialized1")
+  object TestVar2 extends SessionVar[String]("Uninitialized2")
 
   "A FutureWithSession" should {
     "fail if session is not available" in {
@@ -35,53 +35,13 @@ object FutureWithSessionSpec extends Specification {
       }
     }
 
-    "have access to session variables in map() chains" in {
-      val session = new LiftSession("Test Session", "", Empty)
+    "have access to session variables in onComplete()" in {
+      implicit val session = new LiftSession("Test Session", "", Empty)
 
-      val future:Future[String] = S.initIfUninitted(session) {
-        TestVar1("map1")
-        TestVar2("map2")
-        Future("something").withCurrentSession
-      }
-      val actual:Future[String] = future
-        .map(_+"-"+TestVar1.get)
-        .map(_+"-"+TestVar2.get)
-      val expected:Option[Try[String]] = Some(Success("something-map1-map2"))
+      val future:Future[String] = Future("onComplete").withImplicitSession
+      future.onComplete { case Success(s) => TestVar1.set(s) }
 
-      actual.value must eventually(beEqualTo(expected))
-    }
-
-    "have access to session variables in flatMap() chains" in {
-      val session = new LiftSession("Test Session", "", Empty)
-
-      val future:Future[String] = S.initIfUninitted(session) {
-        TestVar1("map1")
-        TestVar2("map2")
-        Future("something").withCurrentSession
-      }
-      val actual:Future[String] = future
-        .flatMap{in => val out = in+"-"+TestVar1.get; Future(out)}
-        .flatMap{in => val out = in+"-"+TestVar2.get; Future(out)}
-      val expected:Option[Try[String]] = Some(Success("something-map1-map2"))
-
-      actual.value must eventually(beEqualTo(expected))
-    }
-
-    "have access to session variables in filter() chains" in {
-      val session = new LiftSession("Test Session", "", Empty)
-
-      val future:Future[String] = S.initIfUninitted(session) {
-        TestVar1("map1")
-        TestVar2("map")
-        Future("has map1").withCurrentSession
-      }
-      val actual:Future[String] = future
-        .filter(_.contains(TestVar1.get))
-        .filter(_.contains(TestVar2.get))
-
-      val expected:Option[Try[String]] = Some(Success("has map1"))
-
-      actual.value must eventually(beEqualTo(expected))
+      S.initIfUninitted(session) { TestVar1.get must beEqualTo("onComplete") }
     }
 
     "have access to session variables in andThen() chains" in {
@@ -135,18 +95,175 @@ object FutureWithSessionSpec extends Specification {
       actual.value must eventually(beEqualTo(expected))
     }
 
+    "have access to session variables after calling fallbackTo()" in {
+      val session = new LiftSession("Test Session", "", Empty)
 
-    "initialize with an implicit LiftSession" in {
-      implicit val session = new LiftSession("Test Session", "", Empty)
-      S.initIfUninitted(session) {
-        TestVar1("map")
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("fallbackTo")
+        Future.failed[String](new Exception("failure")).withCurrentSession
       }
-
-      val actual:Future[String] = Future("something").withImplicitSession.map(_+"-"+TestVar1.get)
-      val expected:Option[Try[String]] = Some(Success("something-map"))
+      val actual:Future[String] = future.fallbackTo(Future("success"))
+        .map(_+"-"+TestVar1.get)
+      val expected:Option[Try[String]] = Some(Success("success-fallbackTo"))
 
       actual.value must eventually(beEqualTo(expected))
     }
 
+    "have access to session variables in filter() chains" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("map1")
+        TestVar2("map")
+        Future("has map1").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .filter(_.contains(TestVar1.get))
+        .filter(_.contains(TestVar2.get))
+
+      val expected:Option[Try[String]] = Some(Success("has map1"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "have access to session variables in flatMap() chains" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("map1")
+        TestVar2("map2")
+        Future("something").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .flatMap{in => val out = in+"-"+TestVar1.get; Future(out)}
+        .flatMap{in => val out = in+"-"+TestVar2.get; Future(out)}
+      val expected:Option[Try[String]] = Some(Success("something-map1-map2"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "have access to session variables in foreach()" in {
+      implicit val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = Future("foreach").withImplicitSession
+      future.foreach(TestVar1.set)
+
+      S.initIfUninitted(session) { TestVar1.get } must eventually(beEqualTo("foreach"))
+    }
+
+    "have access to session variables in map() chains" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("map1")
+        TestVar2("map2")
+        Future("something").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .map(_+"-"+TestVar1.get)
+        .map(_+"-"+TestVar2.get)
+      val expected:Option[Try[String]] = Some(Success("something-map1-map2"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "yield another FutureWithSession with mapTo()" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[Object] = S.initIfUninitted(session) {
+        TestVar1("mapTo")
+        Future("something").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .mapTo[String]
+        .map(_+"-"+TestVar1.get)
+      val expected:Option[Try[String]] = Some(Success("something-mapTo"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "have access to session variables in onFailure()" in {
+      implicit val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = Future.failed[String](new Exception("failure")).withImplicitSession
+      future.onFailure { case e:Exception => TestVar1.set(e.getMessage) }
+
+      S.initIfUninitted(session) { TestVar1.get } must eventually(beEqualTo("failure"))
+    }
+
+    "have access to session variables in onSuccess()" in {
+      implicit val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = Future("onSuccess").withImplicitSession
+      future.onSuccess { case s => TestVar1.set(s) }
+
+      S.initIfUninitted(session) { TestVar1.get } must eventually(beEqualTo("onSuccess"))
+    }
+
+    "have access to session variables with recover()" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("recover1")
+        TestVar2("recover2")
+        Future.failed[String](new Exception("failure")).withCurrentSession
+      }
+      val actual:Future[String] = future
+        .recover { case e:Throwable => e.getMessage+"-"+TestVar1.get }
+        .map(_+"-"+TestVar2.get)
+      val expected:Option[Try[String]] = Some(Success("failure-recover1-recover2"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "have access to session variables with recoverWith()" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("recover1")
+        TestVar2("recover2")
+        Future.failed[String](new Exception("failure")).withCurrentSession
+      }
+      val actual:Future[String] = future
+        .recoverWith { case e:Throwable => val out = e.getMessage+"-"+TestVar1.get; Future(out) }
+        .map(_+"-"+TestVar2.get)
+      val expected:Option[Try[String]] = Some(Success("failure-recover1-recover2"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "have access to session variables with transform()" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("transform1")
+        TestVar2("transform2")
+        Future("orig").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .transform( s => throw new Exception(TestVar1.get), identity[Throwable] )
+        .transform( identity[String], t => new Exception(t.getMessage+"-"+TestVar2.get) )
+        .recover { case e:Exception => e.getMessage }
+
+      val expected:Option[Try[String]] = Some(Success("transform1-transform2"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
+
+    "yield another FutureWithSession with zip()" in {
+      val session = new LiftSession("Test Session", "", Empty)
+
+      val future:Future[String] = S.initIfUninitted(session) {
+        TestVar1("zip")
+        Future("one").withCurrentSession
+      }
+      val actual:Future[String] = future
+        .zip(Future("two"))
+        .collect { case (one, two) => one+"-"+TestVar1.get+"-"+two }
+
+      val expected:Option[Try[String]] = Some(Success("one-zip-two"))
+
+      actual.value must eventually(beEqualTo(expected))
+    }
   }
 }
