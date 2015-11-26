e05ea54f4537fdcf699cd016f8869972bd42a35d
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2015-11-25 23:20:15 -0500

    Place credentials where sbt's lookin'.
    We were going to ~/.credentials, but that's not where sbt is looking for them!

--- travis.sh
+++ travis.sh
@@ -5,7 +5,8 @@ set -ev
 ./liftsh test
 
 if [ "${TRAVIS_PULL_REQUEST}" = "1729" ]; then
-  openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.credentials -d
+  mkdir -p ~/.sbt/0.13/
+  openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/0.13/.credentials -d
 
   if [ "${TRAVIS_BRANCH}" = "master" ]; then
     ./liftsh publish
@@ -14,5 +15,5 @@ if [ "${TRAVIS_PULL_REQUEST}" = "1729" ]; then
     ./liftsh ++2.11.1 publish
   fi
 
-  rm ~/.credentials
+  rm ~/.sbt/0.13/.credentials
 fi
