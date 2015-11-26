20d25921e6ea33dee6463985e664a74ce2e7f63c
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2015-11-26 10:13:07 -0500

    Deploy on non-PR build.
    We were deploying on PR build so we could test things, but now we switch to
    deploying only when the build is a non-PR build.

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -ev
 
 ./liftsh test
 
-if [ "${TRAVIS_PULL_REQUEST}" = "1729" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   mkdir -p ~/.sbt/0.13/
   openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/0.13/.credentials -d
 
