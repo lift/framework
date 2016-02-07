1ecbaa9376827a29a28ddc1e9b64bbe30ec611fe
Matt Farmer <matt@frmr.me>
2016-02-07 00:15:26 -0500

    Pull mkMapping out into its own first-class function.

--- core/json/src/main/scala/net/liftweb/json/Extraction.scala
+++ core/json/src/main/scala/net/liftweb/json/Extraction.scala
@@ -187,18 +187,18 @@ object Extraction {
     }
   }
 
+  private def mkMapping(clazz: Class[_], typeArgs: Seq[Class[_]])(implicit formats: Formats): Meta.Mapping = {
+    if (clazz == classOf[Option[_]] || clazz == classOf[List[_]] || clazz == classOf[Set[_]] || clazz.isArray) {
+      Col(TypeInfo(clazz, None), mkMapping(typeArgs.head, typeArgs.tail))
+    } else if (clazz == classOf[Map[_, _]]) {
+      Dict(mkMapping(typeArgs.tail.head, typeArgs.tail.tail))
+    } else {
+      mappingOf(clazz, typeArgs)
+    }
+  }
+
   private def extract0(json: JValue, clazz: Class[_], typeArgs: Seq[Class[_]])
                       (implicit formats: Formats): Any = {
-    def mkMapping(clazz: Class[_], typeArgs: Seq[Class[_]])(implicit formats: Formats): Meta.Mapping = {
-      if (clazz == classOf[Option[_]] || clazz == classOf[List[_]] || clazz == classOf[Set[_]] || clazz.isArray) {
-        Col(TypeInfo(clazz, None), mkMapping(typeArgs.head, typeArgs.tail))
-      } else if (clazz == classOf[Map[_, _]]) {
-        Dict(mkMapping(typeArgs.tail.head, typeArgs.tail.tail))
-      } else {
-        mappingOf(clazz, typeArgs)
-      }
-    }
-
     extract0(json, mkMapping(clazz, typeArgs))
   }
 
