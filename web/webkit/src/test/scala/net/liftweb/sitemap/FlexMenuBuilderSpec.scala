82d024fc2582dd4df6762f0a1c76aa5b15132d0b
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2016-03-13 18:55:56 -0400

    WebSpec correctly adds fragments it creates.
    The WebSpec extension classes like SessionSpecification were creating
    fragments, but weren't adding them to the fragments to run, so those
    tests were never getting executed.
    
    We now properly add the generated fragments to the list of fragments to run.

--- web/webkit/src/test/scala/net/liftweb/sitemap/FlexMenuBuilderSpec.scala
+++ web/webkit/src/test/scala/net/liftweb/sitemap/FlexMenuBuilderSpec.scala
@@ -36,12 +36,14 @@ object FlexMenuBuilderSpec extends WebSpec(FlexMenuBuilderSpecBoot.boot _) {
       val actual = MenuBuilder.render
       linkToSelf must beEqualToIgnoringSpace(actual)
     }
+
     "expandAll" withSFor(testUrl) in {
       object MenuBuilder extends FlexMenuBuilder { override def expandAll = true}
       val expandAll: NodeSeq = <ul><li><a href="/index">Home</a></li><li><span>Help</span><ul><li><a href="/index1">Home1</a></li><li><a href="/index2">Home2</a></li></ul></li><li><a href="/help2">Help2</a><ul><li><a href="/index3">Home3</a></li><li><a href="/index4">Home4</a></li></ul></li></ul>
       val actual = MenuBuilder.render
       expandAll.toString must_== actual.toString
     }
+
     "Add css class to item in the path" withSFor(testUrlPath) in {
       object MenuBuilder extends FlexMenuBuilder {
         override def updateForPath(nodes: Elem, path: Boolean): Elem = {
@@ -56,6 +58,7 @@ object FlexMenuBuilderSpec extends WebSpec(FlexMenuBuilderSpecBoot.boot _) {
       val actual = MenuBuilder.render
       itemInPath.toString must_== actual.toString
     }
+
     "Add css class to the current item" withSFor(testUrl) in {
       object MenuBuilder extends FlexMenuBuilder {
         override def updateForCurrent(nodes: Elem, current: Boolean): Elem = {
