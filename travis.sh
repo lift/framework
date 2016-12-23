e2552c6676c294b871a9158fffdea1fd91d28a2f
Matt Farmer <matt@frmr.me>
2016-12-23 15:25:46 -0500

    Use Travis's built-in sbt.
    Using liftsh will cause us to waste time downloading and setting up our
    own sbt when Travis already has one in the image they're executing.

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-./liftsh ++$TRAVIS_SCALA_VERSION test
+sbt ++$TRAVIS_SCALA_VERSION test
 
 ./npmsh
 
@@ -10,7 +10,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   mkdir -p ~/.sbt/0.13/
   openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/0.13/.credentials -d
 
-  ./liftsh ++$TRAVIS_SCALA_VERSION publish
+  sbt ++$TRAVIS_SCALA_VERSION publish
 
   rm ~/.sbt/0.13/.credentials
 fi
