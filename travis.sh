c3b51ccfe21fdc1eb1377c6fedb5feeeb3af974c
Matt Farmer <matt@frmr.me>
2016-12-23 15:41:58 -0500

    Implement the workaround for scala/scala-dev#249

--- travis.sh
+++ travis.sh
@@ -10,7 +10,9 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   mkdir -p ~/.sbt/0.13/
   openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/0.13/.credentials -d
 
-  sbt ++$TRAVIS_SCALA_VERSION publish
+  # Include the no-java-comments work-around so that publishing for Scala 2.12 will work correctly.
+  # See: https://github.com/scala/scala-dev/issues/249
+  sbt ++$TRAVIS_SCALA_VERSION "set scalacOptions in (Compile, doc) += \"-no-java-comments\"" publish
 
   rm ~/.sbt/0.13/.credentials
 fi
