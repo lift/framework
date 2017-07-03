46bb3a825bb4f8b118c65da3c17b33b438d67698
bhashit parikh <bhashit.parikh@gmail.com>
2017-07-03 20:25:07 +0530

    Separate out the test for the headers into three seperate parts. Set the default value for the `headers` param in `getRequestSnapshot` to Nil

--- web/webkit/src/test/scala/net/liftweb/http/provider/servlet/OfflineRequestSnapshotSpec.scala
+++ web/webkit/src/test/scala/net/liftweb/http/provider/servlet/OfflineRequestSnapshotSpec.scala
@@ -25,26 +25,36 @@ import org.specs2.mock.Mockito
 object OfflineRequestSnapshotSpec extends WebSpec with Mockito {
 
   private[this] val X_SSL = "X-SSL"
+  private[this] val xSSLHeader = HTTPParam(X_SSL, List("true")) :: Nil
 
   "OfflineRequestSnapshot" should {
     "have a 'headers' method that returns the list of headers with a given name" in {
-      val req = getRequestSnapshot(originalPort = 80)
+      val req = getRequestSnapshot(originalPort = 80, headers = xSSLHeader)
       req.headers("X-SSL") shouldEqual List("true")
       req.headers("Unknown") must beEmpty
     }
 
     "have the serverPort value" in {
       "443 when the 'X-SSL' header is set to the string 'true' (case-insensitive) and original port is 80" in {
-        val port80Req = getRequestSnapshot(originalPort = 80)
+        val port80Req = getRequestSnapshot(originalPort = 80, headers = xSSLHeader)
         port80Req.serverPort shouldEqual 443
       }
-      s"equal to the original request-port when the '$X_SSL' header is absent or not set to the string 'true' (case-insensitive), or if the original port is not 80" in {
-        val req = getRequestSnapshot(originalPort = 90)
-        val nonSSLReq = getRequestSnapshot(originalPort = 80, headers =  Nil)
-        val falseSSLHeaderReq = getRequestSnapshot(originalPort = 90, headers =  HTTPParam(X_SSL, List("anything")) :: Nil)
-        req.serverPort shouldEqual 90
-        nonSSLReq.serverPort shouldEqual 80
-        falseSSLHeaderReq.serverPort shouldEqual 90
+
+      s"equal to the original request-port when" in {
+        s"the '$X_SSL' header is absent" in {
+          val nonSSLReq = getRequestSnapshot(originalPort = 80)
+          nonSSLReq.serverPort shouldEqual 80
+        }
+
+        s"the '$X_SSL' header is not set to the string 'true' (case-insensitive)" in {
+          val falseSSLHeaderReq = getRequestSnapshot(originalPort = 90, headers =  HTTPParam(X_SSL, List("anything")) :: Nil)
+          falseSSLHeaderReq.serverPort shouldEqual 90
+        }
+
+        "the original request-port is not 80" in {
+          val req = getRequestSnapshot(originalPort = 90, headers = xSSLHeader)
+          req.serverPort shouldEqual 90
+        }
       }
     }
 
@@ -60,9 +70,8 @@ object OfflineRequestSnapshotSpec extends WebSpec with Mockito {
     }
   }
 
-  private[this] val xSSLHeader = HTTPParam(X_SSL, List("true")) :: Nil
 
-  private[this] def getRequestSnapshot(originalPort: Int, headers: List[HTTPParam] = xSSLHeader, params: List[HTTPParam] = Nil) = {
+  private[this] def getRequestSnapshot(originalPort: Int, headers: List[HTTPParam] = Nil, params: List[HTTPParam] = Nil) = {
     val mockHttpRequest = mock[HTTPRequest]
     val httpProvider = new HTTPProvider {
       override protected def context: HTTPContext = null
@@ -76,4 +85,3 @@ object OfflineRequestSnapshotSpec extends WebSpec with Mockito {
   }
 
 }
-
