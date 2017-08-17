b64c5fec120450accbf65180b93f5e42b41a5d39
Joe Barnes <joe@joescii.com>
2017-08-17 09:09:32 -0500

    Removing the LiftRulesGuardedSetting check for a setting being set multiple times

--- web/webkit/src/main/scala/net/liftweb/http/LiftRulesGuardedSetting.scala
+++ web/webkit/src/main/scala/net/liftweb/http/LiftRulesGuardedSetting.scala
@@ -33,12 +33,6 @@ object LiftRulesGuardedSetting {
     */
   case class SettingWrittenAfterBoot(settingName: String, stackTrace: StackTrace, message: String)
     extends SettingViolation(settingName, stackTrace, message)
-
-  /**
-    * Indicates that a LiftRulesGuardedSetting was set to two different values.
-    */
-  case class SettingWrittenTwice(settingName: String, stackTrace: StackTrace, message: String)
-    extends SettingViolation(settingName, stackTrace, message)
 }
 
 import LiftRulesGuardedSetting._
@@ -64,14 +58,6 @@ class LiftRulesGuardedSetting[T](val name: String, val default: T) extends LiftV
     s"LiftRules.$name set after Lift finished booting. " +
     s"Review the stacktrace below to see where settings are being changed after boot time. "
 
-  private[this] def writtenTwiceMessage1(newVal: T) =
-    s"LiftRules.$name was set to $v then later set to $newVal. " +
-    s"This could potentially cause your Lift application to run in an inconsistent state. " +
-    s"Review the stacktrace below to see where LiftRules.$name was first set to $v. "
-
-  private[this] def writtenTwiceMessage2(newVal: T) =
-    s"Review the stacktrace below to see where LiftRules.$name was later set to $newVal. "
-
   private[this] def trimmedStackTrace(t: Throwable): StackTrace = {
     val toIgnore = Set("LiftRulesGuardedSetting", "LiftValue")
     t.getStackTrace.dropWhile(e => toIgnore.find(e.getClassName contains _).isDefined)
@@ -80,14 +66,6 @@ class LiftRulesGuardedSetting[T](val name: String, val default: T) extends LiftV
   private[this] def currentStackTrace: StackTrace = trimmedStackTrace(new Exception)
 
   override def set(value: T): T = {
-    lastSet.foreach { case stackTrace if v != value =>
-      val e1 = SettingWrittenTwice(name, stackTrace, writtenTwiceMessage1(value))
-      LiftRules.guardedSettingViolationFunc.get.apply(e1)
-
-      val e2 = SettingWrittenTwice(name, currentStackTrace, writtenTwiceMessage2(value))
-      LiftRules.guardedSettingViolationFunc.get.apply(e2)
-    }
-
     if(LiftRules.doneBoot) {
       val e = SettingWrittenAfterBoot(name, currentStackTrace, writeAfterBootMessage)
       LiftRules.guardedSettingViolationFunc.get.apply(e)
