0670b7643dfcdd210c755ff176109e4abc5de435
Matt Farmer <matt@frmr.me>
2017-08-19 08:41:17 -0400

    Move MatchingModule to its own file

--- project/Build.scala
+++ project/Build.scala
@@ -22,17 +22,6 @@ import Dependencies._
 import com.typesafe.sbt.web.SbtWeb
 import com.typesafe.sbt.web.SbtWeb.autoImport._
 
-/**
- * Pattern-matches an attributed file, extracting its module organization,
- * name, and revision if available in its attributes.
- */
-object MatchingModule {
-  def unapply(file: Attributed[File]): Option[(String,String,String)] = {
-    file.get(moduleID.key).map { moduleInfo =>
-      (moduleInfo.organization, moduleInfo.name, moduleInfo.revision)
-    }
-  }
-}
 
 object BuildDef extends Build {
 
