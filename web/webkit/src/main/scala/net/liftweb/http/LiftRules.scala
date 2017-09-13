ea9e33faa296ffca39837e472fe0c4746072e934
Matt Farmer <matt@frmr.me>
2017-09-12 22:14:03 -0400

    Back out the urlencode when passing context path to client

--- web/webkit/src/main/scala/net/liftweb/http/LiftRules.scala
+++ web/webkit/src/main/scala/net/liftweb/http/LiftRules.scala
@@ -766,7 +766,7 @@ class LiftRules() extends Factory with FormVendor with LazyLoggable {
    * lift.cometOnSessionLost reloads the current page by default.
    */
   val noCometSessionCmd = new FactoryMaker[JsCmd](
-    () => JsCmds.Run(s"lift.cometOnSessionLost('${urlEncode(S.contextPath).replace("'", "\\'")}')")
+    () => JsCmds.Run(s"lift.cometOnSessionLost('${S.contextPath.replace("'", "\\'")}')")
   ) {}
 
   /**
