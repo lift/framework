271d6296c58d9b31cfa0f2f0c57fa89b8a57ee20
Matt Farmer <matt@frmr.me>
2019-07-13 16:58:06 -0400

    Try moving the credentials to ~/.sbt/1.0

--- travis.sh
+++ travis.sh
@@ -7,10 +7,10 @@ sbt ++$TRAVIS_SCALA_VERSION test
 ./npmsh
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  mkdir -p ~/.sbt/0.13/
-  openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/0.13/.credentials -d
+  mkdir -p ~/.sbt/1.0/
+  openssl aes-256-cbc -K $encrypted_a177bbd76133_key -iv $encrypted_a177bbd76133_iv -in .credentials.enc -out ~/.sbt/1.0/.credentials -d
 
   sbt ++$TRAVIS_SCALA_VERSION publish
 
-  rm ~/.sbt/0.13/.credentials
+  rm ~/.sbt/1.0/.credentials
 fi
